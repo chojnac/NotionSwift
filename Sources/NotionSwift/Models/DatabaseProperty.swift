a4e92ff0054946127fbc167b036617cd522b6211
Michał Żołnieruk <gierekg@gmail.com>
2022-07-01 16:51:55 +0200

    Included Name property in DatabaseProperty type

--- Sources/NotionSwift/Models/DatabaseProperty.swift
+++ Sources/NotionSwift/Models/DatabaseProperty.swift
@@ -7,13 +7,16 @@ import Foundation
 public struct DatabaseProperty {
     public typealias Identifier = EntityIdentifier<DatabaseProperty, String>
     public let id: Identifier
+    public let name: String
     public let type: DatabasePropertyType
 
     public init(
         id: DatabaseProperty.Identifier,
+        name: String,
         type: DatabasePropertyType
     ) {
         self.id = id
+        self.name = name
         self.type = type
     }
 }
@@ -21,11 +24,13 @@ public struct DatabaseProperty {
 extension DatabaseProperty: Decodable {
     enum CodingKeys: String, CodingKey {
         case id
+        case name
     }
 
     public init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         self.id = try container.decode(Identifier.self, forKey: .id)
+        self.name = try container.decode(String.self, forKey: .name)
         self.type = try DatabasePropertyType(from: decoder)
     }
 }
