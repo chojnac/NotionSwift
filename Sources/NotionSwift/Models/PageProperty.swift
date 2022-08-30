34c271362c54e32abb2a4389057379e1f04d1fac
Michał Żołnieruk <gierekg@gmail.com>
2022-08-30 17:41:14 +0200

    Revert

--- Sources/NotionSwift/Models/PageProperty.swift
+++ Sources/NotionSwift/Models/PageProperty.swift
@@ -240,7 +240,7 @@ extension PagePropertyType: Codable {
                 forKey: .url
             )
             if let value = value) {
-                self = .url(url)
+                self = .url(URL(string: value))           
             } else {
                 self = .url(nil)
             }
