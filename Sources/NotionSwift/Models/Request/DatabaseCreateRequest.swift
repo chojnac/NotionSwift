cf08a276d7dad4ee78d6fcb17c11749b9d84a5ab
Patrix <champion790713@gmail.com>
2023-04-11 02:40:38 +0800

    Add the `isInline` property to create inline table (#42)
    * add the `isInline` property to create inline table
    * Add the `isInline` property to the `Database` entity

--- Sources/NotionSwift/Models/Request/DatabaseCreateRequest.swift
+++ Sources/NotionSwift/Models/Request/DatabaseCreateRequest.swift
@@ -10,19 +10,22 @@ public struct DatabaseCreateRequest {
     public let cover: CoverFile?
     public let title: [RichText]?
     public let properties: [Database.PropertyName: DatabasePropertyType]
+    public let isInline: Bool?
 
     public init(
         parent: DatabaseParent,
         icon: IconFile?,
         cover: CoverFile?,
         title: [RichText]?,
-        properties: [Database.PropertyName: DatabasePropertyType]
+        properties: [Database.PropertyName: DatabasePropertyType],
+        isInline: Bool? = false
     ) {
         self.parent = parent
         self.icon = icon
         self.cover = cover
         self.title = title
         self.properties = properties
+        self.isInline = isInline
     }
 }
 
@@ -33,6 +36,7 @@ extension DatabaseCreateRequest: Encodable {
         case cover
         case title
         case properties
+        case isInline = "is_inline"
     }
 
     public func encode(to encoder: Encoder) throws {
@@ -42,5 +46,6 @@ extension DatabaseCreateRequest: Encodable {
         try container.encodeIfPresent(cover, forKey: .cover)
         try container.encodeIfPresent(title, forKey: .title)
         try container.encode(properties, forKey: .properties)
+        try container.encode(isInline, forKey: .isInline)
     }
 }
